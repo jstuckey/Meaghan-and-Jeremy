3c88b08eebd63d01b4b941b3beb4a778546bba53
Jeremy Stuckey <jbstucke@Jeremy-Stuckeys-MacBook-Pro.local>
2012-09-25 17:40:39 -0400

    Scrolling test.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
+
+
 function clickedPanel(panelName) {
     // Hide the previous html animated, load the new html, show animated
     $('article').hide("1", function() { $('article').load('html/' + panelName + '.html', '', function() { $('article').show("1"); }); });
+    
+    // $('html,body').animate({scrollTop: $("#article").offset().top},'slow');
 }
