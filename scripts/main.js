72efebfcacacc52a47156c94e8add2d76412ee0f
Jeremy Stuckey <jbstucke@Jeremy-Stuckeys-MacBook-Pro.local>
2012-09-26 17:21:20 -0400

    Added scrolling to article.

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,9 @@
 
 function clickedPanel(panelName) {
     // Hide the previous html animated, load the new html, show animated
-    $('article').hide("1", function() { $('article').load('html/' + panelName + '.html', '', function() { $('article').show("1"); }); });
-    
-    // $('html,body').animate({scrollTop: $("#article").offset().top},'slow');
+    $('article').hide("1", function() { $('article').load('html/' + panelName + '.html', '', function() { $('article').show("1", function() { scrollToArticle(); }); }); });
+}
+
+function scrollToArticle() {
+     $('html,body').animate({scrollTop: $("#article").offset().top}, 200);
 }
